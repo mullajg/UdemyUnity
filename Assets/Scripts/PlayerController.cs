6520a5bcdb552859640f2f7295b566d35eadd95e
mullajg <mullajg@gmail.com>
2023-11-21 23:13:00 -0500

    Player movement animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,15 @@ public class PlayerController : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -30,6 +34,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         PlayerInput();
+        AdjustPlayerFacingDirection();
     }
 
     //FixedUpdate for physics
@@ -41,10 +46,27 @@ public class PlayerController : MonoBehaviour
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
 
     private void Move()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        if(mousePosition.x < playerScreenPoint.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        if (mousePosition.x > playerScreenPoint.x)
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 }
